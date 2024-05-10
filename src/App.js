4d5a82b1a5f24566179b5ae3efe2b3a5dbfa3655
Charly M <chrlmztt@gmail.com>
2024-05-10 10:35:24 +0200

    btns

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./index.css";
-import deps from './deps.json';
-import Confetti from 'react-confetti';
+import deps from "./deps.json";
+import Confetti from "react-confetti";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { FiRefreshCcw } from "react-icons/fi";
 
 function App() {
   const [currentDep, setCurrentDep] = useState(null);
-  const [userChoice, setUserChoice] = useState('');
+  const [userChoice, setUserChoice] = useState("");
   const [isCorrect, setIsCorrect] = useState(null);
   const [showCorrect, setShowCorrect] = useState(false);
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
+  const [isSpoilerOpen, setIsSpoilerOpen] = useState(false);
+
   useEffect(() => {
     const handleResize = () => {
       setWindowSize({
@@ -20,10 +24,10 @@ function App() {
       });
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
   // Choisissez un département aléatoire au chargement de la page
@@ -35,6 +39,7 @@ function App() {
   const shuffleDep = () => {
     const randomDep = deps[Math.floor(Math.random() * deps.length)];
     setCurrentDep(randomDep);
+    setIsSpoilerOpen(false); // Ajoutez cette ligne
   };
 
   const checkAnswer = () => {
@@ -45,7 +50,7 @@ function App() {
       setTimeout(() => setShowCorrect(false), 3000);
       shuffleDep();
       setIsCorrect(null);
-      setUserChoice('');
+      setUserChoice("");
     } else {
       setIsCorrect(false);
     }
@@ -53,39 +58,62 @@ function App() {
 
   return (
     <div className="App flex min-h-screen bg-gray-50">
-        <div className="flex flex-col w-fit m-auto items-center justify-center gap-4 text-center">
-      {currentDep && (
-        <>
-          {showCorrect && (
-            <div 
-              className="h-screen w-screen flex justify-center pt-24 absolute backdrop-blur-lg text-3xl text-green-500 font-bold transition transform duration-200 ease-out scale-100 opacity-100"
-              onClick={() => setShowCorrect(false)} // Ajoutez cette ligne
-            >
-              <Confetti
-                width={windowSize.width}
-                height={windowSize.height}
-              />
-              Correct !
+      <div className="flex flex-col w-fit m-auto items-center justify-center gap-4 text-center">
+        {currentDep && (
+          <>
+            {showCorrect && (
+              <div
+                className="h-screen w-screen flex justify-center items-center pt-24 absolute backdrop-blur-lg text-3xl text-green-500 font-bold transition transform duration-200 ease-out scale-100 opacity-100 z-50"
+                onClick={() => setShowCorrect(false)} // Ajoutez cette ligne
+              >
+                <Confetti width={windowSize.width} height={windowSize.height} />
+                Correct !
+              </div>
+            )}
+            <p className="text-x font-bold">
+              Quel est le nom du département numéro {currentDep.code} ?
+            </p>
+            <input
+              list="dep-names"
+              value={userChoice}
+              onChange={(e) => setUserChoice(e.target.value)}
+              placeholder="Réponse"
+              className={`${
+                isCorrect === true
+                  ? "border-4 border-green-500"
+                  : isCorrect === false
+                  ? "border-4 border-red-500"
+                  : ""
+              }`}
+            />
+            <datalist id="dep-names">
+              {deps.map((dep, index) => (
+                <option key={index} value={dep.nom} />
+              ))}
+            </datalist>
+            <div className="flex gap-4">
+                         <button onClick={checkAnswer} className="btn">
+              Vérifier
+            </button>
+            <button onClick={shuffleDep} className="btn-outline">
+              <FiRefreshCcw />
+            </button> 
             </div>
-          )}
-          <p className="text-x font-bold">Quel est le nom du département numéro {currentDep.code} ?</p>
-          <input list="dep-names" value={userChoice} onChange={e => setUserChoice(e.target.value)} placeholder="Réponse"
-             className={`${isCorrect === true ? 'border-4 border-green-500' : isCorrect === false ? 'border-4 border-red-500' : ''}`}
-          />
-          <datalist id="dep-names">
-            {deps.map((dep, index) => (
-              <option key={index} value={dep.nom} />
-            ))}
-          </datalist>
-          <button onClick={checkAnswer} className="btn">Vérifier</button>
-          <button onClick={shuffleDep} className="btn-outline">Changer de département</button>
-          <details className="cursor-pointer">
-            <summary>Réponse</summary>
-            <p className="text-green-500 font-bold">{currentDep.nom}</p>
-          </details>
-        </>
-      )}
-    </div>
+
+            <div
+              className="cursor-pointer"
+              onClick={() => setIsSpoilerOpen(!isSpoilerOpen)}>
+              <div className="flex items-center gap-4">
+                Réponse
+                {isSpoilerOpen ? <FiChevronUp /> : <FiChevronDown />}
+              </div>
+              {isSpoilerOpen && (
+                <div className="text-green-500 font-bold">{currentDep.nom}</div>
+              )}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
