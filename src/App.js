b5459b2518b3e2f34a1eb4c9c0c77a93b6fa9984
Charly M <chrlmztt@gmail.com>
2024-05-10 09:55:01 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./index.css";
+import deps from './deps.json';
 
 function App() {
+  const [currentDep, setCurrentDep] = useState(null);
+  const [userChoice, setUserChoice] = useState('');
+
+  // Choisissez un département aléatoire au chargement de la page
+  useEffect(() => {
+    shuffleDep();
+  }, []);
+
+  const shuffleDep = () => {
+    const randomDep = deps[Math.floor(Math.random() * deps.length)];
+    setCurrentDep(randomDep);
+  };
+
+  const checkAnswer = () => {
+    if (userChoice === currentDep.nom) {
+      alert('Correct !');
+      shuffleDep();
+      setUserChoice('');
+    } else {
+      alert('Incorrect, essayez encore.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex min-h-screen bg-gray-50">
+        <div className="flex flex-col w-fit m-auto items-center justify-center gap-4 text-center">
+      {currentDep && (
+        <>
+          <p className="text-xl">Quel est le nom du département numéro {currentDep.code} ?</p>
+          <input list="dep-names" value={userChoice} onChange={e => setUserChoice(e.target.value)} placeholder="Réponse"/>
+          <datalist id="dep-names">
+            {deps.map((dep, index) => (
+              <option key={index} value={dep.nom} />
+            ))}
+          </datalist>
+          <button onClick={checkAnswer} className="btn">Vérifier</button>
+          <button onClick={shuffleDep} className="btn-outline">Changer de département</button>
+          <details className="cursor-pointer">
+            <summary>Réponse</summary>
+            <p>{currentDep.nom}</p>
+          </details>
+        </>
+      )}
+    </div>
     </div>
   );
 }
